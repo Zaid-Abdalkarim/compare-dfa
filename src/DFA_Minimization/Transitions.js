01dca380381e33108fbb63d9524bd63400d44dc9
SergioRamirez1001 <sergio.ramirez.754@my.csun.edu>
2023-12-08 00:42:01 -0800

    Adding some domain files to help with operations

---
+++ src/DFA_Minimization/Transitions.js
@@ -0,0 +1,7 @@
+//This will be used to hold a collection of Transitions
+class Transitions {
+    constructor(transitions) {
+        this.transitions = transitions;
+    }
+}
+export default Transitions
